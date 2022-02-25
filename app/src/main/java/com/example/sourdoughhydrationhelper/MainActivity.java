35c149392df21e7fc6fe06db0dfb474e8bc13ea2
herpdaderpin <50278544+RichardSDietz@users.noreply.github.com>
2022-02-24 20:16:49 -0600

    Added in basic UI and completed functionality of calculator to get the app to a basic functional form.

--- app/src/main/java/com/example/sourdoughhydrationhelper/MainActivity.java
+++ app/src/main/java/com/example/sourdoughhydrationhelper/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sourdoughhydrationhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculateHydration(View view)   {
+
+        EditText flourText = findViewById(R.id.flourText);
+        EditText hydrationText = findViewById(R.id.hydrationText);
+        TextView flourCalcText = findViewById(R.id.flourCalcText);
+        TextView waterCalcText = findViewById(R.id.waterCalcText);
+        TextView starterCalcText = findViewById(R.id.starterCalcText);
+        TextView saltCalcText = findViewById(R.id.saltCalcText);
+
+        String flourAmountS = flourText.getText().toString();
+        Float flourAmountF = Float.parseFloat(flourAmountS);
+        String hydrationPercS = hydrationText.getText().toString();
+        Float hydrationPercF = Float.parseFloat(hydrationPercS);
+        Float starterAmountF = flourAmountF * .28f;
+        Float waterAmountF = ((flourAmountF + (starterAmountF / 2)) * (hydrationPercF / 100)) - (starterAmountF / 2);
+        Float saltAmountF = (flourAmountF + (starterAmountF / 2)) * .02f;
+        flourCalcText.setText(String.format(Locale.ENGLISH, "%.0f", flourAmountF));
+        waterCalcText.setText(String.format(Locale.ENGLISH, "%.0f", waterAmountF));
+        starterCalcText.setText(String.format(Locale.ENGLISH, "%.0f", starterAmountF));
+        saltCalcText.setText(String.format(Locale.ENGLISH, "%.0f", saltAmountF));
     }
 }
